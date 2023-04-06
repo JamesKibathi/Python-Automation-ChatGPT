09f2dd6e33436bedf4f56ba4cbd4ab1c22699621
James Njenga <knjenga94@gmail.com>
2023-04-06 20:44:17 +0300

    Logic to connect to OpenAI, Accept prompt input and send a reply

---
+++ python_chatgpt.py
@@ -0,0 +1,42 @@
+import requests
+import argparse
+import os
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="The prompt to send to the OpenAI API")
+parser.add_argument("file_name", help="Name of the file to save Python script")
+args = parser.parse_args()
+
+api_endpoint = 'https://api.openai.com/v1/completions'
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+request_headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + api_key
+}
+
+request_data = {
+    "model": "text-davinci-003",
+    "prompt": f"Write python script to {args.prompt}",
+    "max_tokens": 500,
+    "temperature": 0.5
+}
+# Set the maximum number of retries and the time to wait between retries
+max_retries = 3
+retry_delay = 5  # seconds
+
+for i in range(max_retries):
+    response = requests.post(api_endpoint, headers=request_headers, json=request_data)
+    if response.status_code == 200:
+        response_text = response.json()["choices"][0]["text"]
+        with open(args.file_name, "w") as file:
+            file.write(response_text)
+        break
+    elif response.status_code == 429:
+        print("Request rate-limited. Retrying in {} seconds...".format(retry_delay))
+        time.sleep(retry_delay)
+    else:
+        print(f"Request failed with status code:{response.status_code}")
+        break
